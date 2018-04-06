aeead49b8071b9514d2bdbf9a57ea2eacb8b8e81
hartmetzls <hartmetzls@gmail.com>
2018-04-06 16:36:40 -0400

    Debugging NoneTypes in CQT complete. Model architecture definition in progress: input shape for first layer in progress

--- pre_model.py
+++ pre_model.py
@@ -26,11 +26,12 @@ def split_and_model_architecture(cqt_segments, midi_segments): #TODO: Validation
     cqt_train, cqt_test, midi_train, midi_test = train_test_split(cqt_segments, midi_segments, test_size=0.25, random_state=21) #shuffles before splitting
     example_cqt_segment = cqt_segments[0]
     input_height, input_width = example_cqt_segment.shape
+    num_training_cqts = len(cqt_train)
     model = Sequential()
 
     # Define your architecture.
     model.add(Conv2D(filters=16, kernel_size=2, padding='valid', activation='relu',
-                     input_shape=(input_height, input_width, 1)))
+                     input_shape=(input_height, input_width, None))) #173, 84
     model.add(MaxPooling2D(pool_size=2))
     model.add(Conv2D(filters=32, kernel_size=2, padding='valid', activation='relu'))
     model.add(MaxPooling2D(pool_size=2))
@@ -38,23 +39,19 @@ def split_and_model_architecture(cqt_segments, midi_segments): #TODO: Validation
     model.add(MaxPooling2D(pool_size=2))
     model.add(GlobalAveragePooling2D(data_format='channels_last'))
     model.add(Dense(133, activation='softmax'))
-
     model.summary()
 
-    model.compile('mean_squared_error',
+    model.compile(loss='mean_squared_error',
               optimizer='sgd', metrics=['acc'])
 
     from keras.callbacks import ModelCheckpoint
-
     # specify the number of epochs that you would like to use to train the model.
     epochs = 100
-
     checkpointer = ModelCheckpoint(filepath='saved_models/weights.best.from_scratch.hdf5',
                                    verbose=1, save_best_only=True)
 
-    # model.fit(train_tensors, train_targets,
-    #           validation_data=(valid_tensors, valid_targets),
-    #           epochs=epochs, batch_size=20, callbacks=[checkpointer], verbose=1)
+    model.fit(cqt_train, midi_train,
+              epochs=epochs, batch_size=20, callbacks=[checkpointer], verbose=1)
 
 def depickle_and_model_architecture():
     cqt_segments, midi_segments = pickle_if_not_pickled()
