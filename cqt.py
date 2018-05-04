8100cf697d7fec3fcd02806a3e1615ede28e6039
hartmetzls <hartmetzls@gmail.com>
2018-05-03 22:29:02 -0400

    Capstone first submit.

--- cqt.py
+++ cqt.py
@@ -1,36 +1,16 @@
-
 import librosa
-# import librosa.display
-import ntpath
-from mido import MidiFile
 import numpy as np
-# import matplotlib.pyplot as plt
 
 def audio_segments_cqt(audio_segment_time_series, sr):
-    # desired_cqt_length_minus_2 = 48
-    # minimum_factor_cqt_hop_length = 64
-    # x = int(audio_segment_time_series.shape[0]/(
-    #         minimum_factor_cqt_hop_length*desired_cqt_length_minus_2))
-    # hop_length = minimum_factor_cqt_hop_length * x
-
-    #I believe fmin should be set to the Hz val of the lowest MIDI note number in the dataset
-    cqt_of_segment = librosa.cqt(audio_segment_time_series, sr=sr)   #confirm unnecessary: fmin=29.1, sparsity=.99 #,
+    cqt_of_segment = librosa.cqt(audio_segment_time_series, sr=sr)
     print("cqt:", cqt_of_segment.shape)
+    # Remove the imaginary number part of the CQT (ie We don't care where in the wave we are when we start reading it)
     cqt_of_segment_real = cqt_of_segment.real
     cqt_of_segment_copy_real = np.array(cqt_of_segment_real, dtype='float32')
     return cqt_of_segment_copy_real
 
-    # print(cqt_of_segment)
-    # print("Cqt real:", np.array(cqt_of_segment, 'float32'))
-    #We don't care about the imaginary part bc we don't care where in the wave we are when we
-    #  start reading it
-
 def main():
-    # compare_beats("C:/Users/Lilly/audio_and_midi/audio/Bach_BWV871-02_002_20090916-SMD.wav")
-    # compare_beats("C:/Users/Lilly/audio_and_midi/audio/Bartok_SZ080-02_002_20110315-SMD.wav")
-
     audio_segments_cqt()
-
 
 if __name__ == '__main__':
     main()
